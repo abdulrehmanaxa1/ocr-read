41894cfc06e7c8337802bdca8198e95c8c956c84
abdulrehmanaxa1 <abdul.rehman1@axabiztech.com>
2022-11-14 18:23:09 +0500

    updts

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const cors = require('cors');
 const router = require('./route/index.router')
 
 var ocrController= require('./controller/ocr-controller') 
+var app= express();
 
-app.listen(3000, ()=> console.log('Server Started at Port: 3000'));
+app.listen(3000, '127.0.0.1',()=> console.log('Server Started at Port: 3000'));
