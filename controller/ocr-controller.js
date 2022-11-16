9cb3d039bd1bbd6e70332a681415be975b59d95d
abdulrehmanaxa1 <abdul.rehman1@axabiztech.com>
2022-11-16 18:18:50 +0500

    ocr

---
+++ controller/ocr-controller.js
@@ -0,0 +1,27 @@
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient({keyFilename:'./api-key.json'});
+const detectLandmark = async(file_path)=>{
+    const [result] = await client.landmarkDetection('./resources/random-cnic.jpg');
+    // const labels = result.textAnnotations;
+    console.log(result);
+}
+detectLandmark()
+
+
+//   module.exports.ocr= ((req, res, next)=>{
+//     const client = new vision.ImageAnnotatorClient(CONFIG);
+//     async function setEndpoint() {
+//         // Specifies the location of the api endpoint
+//         const clientOptions = {apiEndpoint: CONFIG};
+      
+//         // Creates a client
+//         // Performs text detection on the image file
+//         const [result] = await client.textDetection('./resources/CNIC.png');
+//         const labels = result.textAnnotations;
+//         console.log('Text:');
+//         labels.forEach(label => console.log(label.description));
+//         console.log(labels)
+//       }
+//       setEndpoint();
+//   })
+
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./route/index.router')
-
-var ocrController= require('./controller/ocr-controller') 
+var tesseract = require('./controller/tesseract')
+// var ocrController= require('./controller/ocr-controller') 
 var app= express();
+
 
 app.listen(3000, '127.0.0.1',()=> console.log('Server Started at Port: 3000'));
